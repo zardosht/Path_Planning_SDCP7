2bd5cfa8eb120acd3c3e1f25d042e6acc6920eb9
Zardosht Hodaie <zhodaie@gmail.com>
2020-06-10 13:48:34 +0200

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -165,7 +165,12 @@ int main() {
             ref_vel += 0.224;
           }
 
-
+          // *********************
+          // * Set the knot points for the spline.
+          // * The spline is then used to give us path
+          // * points with a smooth transition between 
+          // * points.
+          // *********************
           vector<double> ptsx;
           vector<double> ptsy;
 
@@ -173,7 +178,7 @@ int main() {
           double ref_y = car_y;
           double ref_yaw = deg2rad(car_yaw);
 
-          // Beginning points for the current path.
+          // Beginning knot points for the spline.
           // Either take the corrent position of the car
           // (if the previous path is consumed, i.e. 
           // prev_size < 2), or take the last two points
@@ -208,7 +213,9 @@ int main() {
             ptsy.push_back(ref_y);
           }
 
-          // 
+          // end knot points for the spline
+          // pick points in the far distance to
+          // have a smooth spline
           vector<double> next_wp0 = getXY(car_s + 30, 
                                           (2.0 + 4.0 * lane), 
                                           map_waypoints_s, 
@@ -232,7 +239,7 @@ int main() {
           ptsy.push_back(next_wp1[1]);
           ptsy.push_back(next_wp2[1]);
 
-          // transform the points in the ego car's 
+          // transform the knot points in the ego car's 
           // coordinate system (the beginning point 
           // becomes (0, 0) and the beginnign yaw becomes
           // 0 degrees and the rest of the points are 
@@ -247,8 +254,13 @@ int main() {
 
           }
           
+          // set the knot points
           tk::spline spl;
           spl.set_points(ptsx, ptsy);
+
+          // *********************
+          // * finished preparing the spline
+          // ********************* 
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
@@ -261,17 +273,30 @@ int main() {
             next_y_vals.push_back(previous_path_y[i]);
           }
 
+          // Define how far to go in the x direction.
+          // We uniformly divide this distance into N points.
+          // That gives us the x coordinates of the path points. 
+          // The y coordinates are calculated by the spline. 
+          // The time step of the simulator is 0.02. That is the 
+          // simulator reachs each point in the path in 0.02 
+          // seconds. The velocity (ref_vel) hence defines how 
+          // dense the points are. Higher velocity means the points
+          // are far apart. Lower velocity means the points are 
+          // dence near eachother.   
+          // The number of points N is then given by deviding the distance 
+          // by the (speed * time_step). 
+
           double target_x = 30.0; //horizon
           double target_y = spl(target_x);
           double target_dist = sqrt(target_x * target_x + target_y * target_y);
+          double ref_vel_m_per_sec = ref_vel / 2.24;
+          double N = target_dist / (0.02 * ref_vel_m_per_sec);
 
           double x_add_on = 0;
-
           // fill the rest of current path (till we have 50 points)
           // with the points interpolated by the spline
           for(int i = 1; i <= 50 - previous_path_x.size(); i++) 
           {
-            double N = target_dist / (0.02 * ref_vel / 2.24);
             double x_point = x_add_on + target_x / N;
             double y_point = spl(x_point);
 
