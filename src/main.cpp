3ebd945b05b4f2bd664d4adf62681aaf88f86d6f
Zardosht Hodaie <zhodaie@gmail.com>
2020-06-10 11:37:14 +0200

    reduce speed to avoid collision with the front car

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,51 @@ int main() {
           
           int prev_size = previous_path_x.size();
 
+          // where will our ego car be at the end of 
+          // previous_path points
+          if (prev_size > 0) {
+            car_s = end_path_s;
+          } 
+
+          bool too_close = false;
+
+          //reduce speed if a car is in our lane and 
+          // we get too close to it
+          for (int i = 0; i < sensor_fusion.size(); ++i) 
+          {
+            // get the d value of the car
+            float d = sensor_fusion[i][6];
+
+            // check if a car is in our lane
+            if (d < (2 + 4.0 * lane + 2) && d > (2 + 4.0 * lane - 2)) 
+            {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx + vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              // projection of s value of the car in the future 
+              // (i.e. when we would have passed through all the 
+              //  points in previous_path)
+              check_car_s += (double)prev_size * 0.02 * check_speed;
+              
+              // gap between ego car and the car in front of us
+              double gap = 30.0;
+              // distance from ego car to the car in front of us
+              double distance = check_car_s - car_s;
+              // if the car is front of us (distance > 0) and our 
+              // distance is less than allowed gap
+              if (distance > 0 && distance < gap)
+              { 
+                too_close = true;
+                // reduce speed
+                ref_vel = 29.5; //mph
+              }
+            }
+          }
+
+
+
 
           vector<double> ptsx;
           vector<double> ptsy;
