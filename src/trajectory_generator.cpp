30eba895fe3ddf67b93aac8e11966b2aa06babea
Zardosht Hodaie <zhodaie@gmail.com>
2020-06-11 19:37:59 +0200

    Dummy trajectory for following the road works with the restructured code

--- src/trajectory_generator.cpp
+++ src/trajectory_generator.cpp
@@ -12,17 +12,6 @@ TrajectoryGenerator::~TrajectoryGenerator() {}
 Trajectory TrajectoryGenerator::generate_trajectory(Behavior behavior, Vehicle& egocar)
 {
     Trajectory trajectory;
-
-    
-    // for (int i = 0; i < NUM_TRAJECTORY_POINTS; ++i)
-    // {
-    //     double new_x = egocar.x + (i * DISTANCE_INCREMENT) * cos(deg2rad(egocar.yaw));
-    //     double new_y = egocar.y + (i * DISTANCE_INCREMENT) * sin(deg2rad(egocar.yaw));
-    //     trajectory.xs.push_back(new_x);
-    //     trajectory.ys.push_back(new_y);
-    // }
-
-    
     for (int i = 0; i < 50; ++i) {
         double next_s = egocar.s + (i + 1) * DISTANCE_INCREMENT;
         double next_d = egocar.d; 
